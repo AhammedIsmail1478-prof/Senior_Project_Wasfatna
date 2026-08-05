f9a1f463211aedbdd287656374938a21b21aa91f
Bilal Mohammad Tofeeq <bilalmohammadtofeeq2003@gmail.com>
2026-08-05 13:31:07 +0300

    Refactor index.php for improved structure and scripts

--- index.php
+++ index.php
@@ -140,7 +140,7 @@ require 'auth.php';
   </aside>
 
   <div id="backdrop" class="backdrop" hidden></div>
-  <script src="app.js?v=5"></script>
+  <script src="app.js?v=6"></script>
 </body>
 </html>
 
